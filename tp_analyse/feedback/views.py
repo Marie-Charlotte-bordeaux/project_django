9c5772251976cba4c33ff7646e5f36fa4ca6273f
mbordeaux <mbordeaux@amx1.io>
2025-07-09 10:26:54 +0200

    classe les feed par ordre decroissant

--- tp_analyse/feedback/views.py
+++ tp_analyse/feedback/views.py
@@ -4,30 +4,24 @@ from .models import Feedback
 from job_record.models import JobRecord
 
 def feedback_list(request, job_id):
-    min_rating = request.GET.get('min_rating')
     job = get_object_or_404(JobRecord, id=job_id)
 
     feedbacks = job.feedbacks.all() # type: ignore
-    
+    feedbacks = feedbacks.order_by('-rating', '-created_at')
+    min_rating = request.GET.get('min_rating')
+    filter_value = None
+
     if min_rating:
         try:
-            min_rating = int(min_rating)
-            feedbacks = feedbacks.filter(rating__gte=min_rating)
+            filter_value  = int(min_rating)
+            feedbacks = feedbacks.filter(rating__gte=filter_value)
         except ValueError:
-            return JsonResponse({"error": "min_rating must be an integer."}, status=400)
+            filter_value = None 
 
-    data = [
-        {
-            "id": f.id,
-            "author": f.author_name.name,
-            "rating": f.rating,
-            "comment": f.comment,
-            "created_at": f.created_at.strftime("%Y-%m-%d %H:%M"),
-        }
-        for f in feedbacks
-    ]
+    context = {
+        'job': job,
+        'feedbacks': feedbacks,
+        'min_rating': min_rating ,
+    }
 
-    return render(request, 'feedback/feedback_list.html', {
-          'job': job,
-          'feedbacks': feedbacks
-      })
+    return render(request, 'feedback/feedback_list.html', context)
